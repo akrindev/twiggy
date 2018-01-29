e0d60dbb3486edc7197618f0e98c2a12dc743707
Syakirin Amin <akrinmin@gmail.com>
2018-01-29 12:40:59 +0700

    auth

--- public/assets/js/app.login.js
+++ public/assets/js/app.login.js
@@ -4,7 +4,8 @@ $("form").submit(function(e){
   
   var me = $(this);
  
-  $("button").addClass('disabled');
+  $('.form-group').removeClass('has-error');
+  $('.text-danger').remove();
   
   
   $.ajax({
@@ -17,8 +18,35 @@ $("form").submit(function(e){
 	  $("#notify").html("<div class='alert alert-info'>Authenticating . . .</div>").slideDown('slow');
     },
     success: function(r){
+      if(r.validation){
+      if(r.status){
       
 	  $("#notify").html("<div class='alert alert-success'>Successfully . . .</div>");
+        
+      setTimeout(function(){
+        window.location.href = '/profile';
+      },2000);
+      } else {
+        
+	  $("#notify").html("<div class='alert alert-danger'>Email or password doesnt match</div>");
+        
+      }
+      } else {
+        
+	  $("#notify").html("<div class='alert alert-danger'>Ups!! something goes wrong</div>");
+        
+        $.each(r.errors, function(key, value) {
+          
+          var element = $('#' + key);
+          element.closest('div.form-group')
+            .removeClass('has-error')
+            .addClass(value.length > 0 ? 'has-error' : 'has-success')
+            .find('.text-danger')
+            .remove();
+          element.after('<span class="text-danger">' + value + "</span>");
+        });
+      }
+      
       $("button").removeClass('disabled');
     },
     error: function(j, s, t){
