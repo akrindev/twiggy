a3519a03ac6e3ec232bf846675f88dc72eb1ab81
Syakirin Amin <akrinmin@gmail.com>
2018-02-09 10:19:13 +0700

    pagination on discussions index

--- application/Controllers/Home.php
+++ application/Controllers/Home.php
@@ -5,15 +5,17 @@ namespace App\Controllers;
 use CodeIgniter\Controller;
 use App\Auth\Auth;
 use App\Models\Discuss;
-
+//use Illuminate\Pagination\Paginator;
+  
 class Home extends Controller
 {
 
   	protected $twig;
   	protected $auth;
   
-  	public function __construct()
+  	public function __construct(...$params)
     {
+      parent::__construct(...$params);
       if(! isset($_SESSION))
       	session()->start();
       
@@ -23,26 +25,48 @@ class Home extends Controller
   
   	public function index()
     {
-      $discusses = Discuss::all();
-    //  print_r($discusses);
+      $page = $this->request->getGet('page') === '' 
+        	? 1
+        	: $this->request->getGet('page') > 0 
+              ? $this->request->getGet('page') 
+              : 1 ;
+      
+      $limit     = 2; // Number of posts on one page
+      $skip      = ($page - 1) * $limit;
+      $count     = Discuss::all()->count(); 
+      $pagination = [
+            'needed'        => $count > $limit,
+          //'count'         => $count,
+            'page'          => $page,
+            'lastpage'      => (ceil($count / $limit) == 0 ? 1 : ceil($count / $limit)),
+          //'limit'         => $limit,
+        ];
       
       return $this->twig->render('discuss.twig',[
-      	'discusses' => $discusses
+          'pagination'    => $pagination,
+      	  'discusses' => Discuss::take($limit)->skip($skip)->get()
+          
       ]);
       
     }
   
   	public function topic($slug)
     {
-      $discuss = Discuss::where('slug',$slug)->get();
+      $discuss = Discuss::where('slug','=',$slug)->first();
+      $comments = $discuss->comments;
       
       if($discuss->count() === 0)
       {
         return redirect('/notfound');
       }
-     // print_r($discuss);
-      return $this->twig->render('discuss_topic.twig',[
-      	'discuss' => $discuss
+     	//print_r($discuss);
+      
+        
+      
+     return $this->twig->render('discuss_topic.twig',[
+      	'discus' => $discuss,
+        'comments' => $comments
+          
       ]);
     }
   
